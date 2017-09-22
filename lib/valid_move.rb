6132e7799cb7f93fbc718ef9a90e7a3e56ebc631
Thaddaeus Caldwell <tpcaldwell@gmail.com>
2017-09-22 19:07:30 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index >= 0 && index <= 8 && position_taken?(board, index)
+  if index >= 0 && index <= 8
      true
+  elsif position_taken?(board, index)
+    false
    end
 end
 
